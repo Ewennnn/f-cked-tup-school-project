11f213b8061180ec9cf8b0e85f9ee4f96f48b7bb
Debian <debian@vps-8d698e10.vps.ovh.net>
2023-04-05 19:37:12 +0000

    initialize server using pm2

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('<h1>Application works !</h1>')
 })
+app.get('/test', (req, res) => {
+  res.send('{message: "successful"}')
+})
 
 app.listen(5000, () => {
     console.log("App listening on port 5000!");

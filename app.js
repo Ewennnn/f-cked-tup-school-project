094694dba416018a6a3fbea911a75fdab66c0b53
Ewennn <ewen.bosquet@gmail.com>
2023-04-07 11:49:18 +0200

    gitlab jobs update production server

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.get('/test', (req, res) => {
   res.send('{message: "successful"}')
 })
 
-app.get('new', (req, res) => {
-  res.send('{message: "new endpoint!"}')
-})
-
 app.listen(5000, () => {
     console.log("App listening on port 5000!");
 })

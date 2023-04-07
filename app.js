96c4914144f0d89b033955de6340cb152fea6e73
Ewennn <ewen.bosquet@gmail.com>
2023-04-07 10:25:55 +0200

    test update

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/test', (req, res) => {
   res.send('{message: "successful"}')
 })
 
+app.get('new', (req, res) => {
+  res.send('{message: "new endpoint!"}')
+})
+
 app.listen(5000, () => {
     console.log("App listening on port 5000!");
 })

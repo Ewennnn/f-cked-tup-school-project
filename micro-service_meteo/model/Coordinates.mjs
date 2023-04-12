36ed3111ce36a494891254949c357932538c6ad7
Ewennn <ewen.bosquet@gmail.com>
2023-04-12 23:56:30 +0200

    add weather entity

--- micro-service_meteo/model/Coordinates.mjs
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-export default class Weather {
-    latitude
-    longitude
-    constructor(obj) {
-        Object.assign(this,obj)
-    }
-}
--- micro-service_meteo/model/Weather.mjs
+++ micro-service_meteo/model/Weather.mjs
@@ -1,10 +1,24 @@
 'use strict'
 export default class Weather {
-    ts
-    temperature
-    rain
-    coordinates // Coordinates Object
+    insee
+    latitude
+    longitude
+    timestamp
+
+    weather
+    temp2m
+    probarain
+    wind10m
+    probawind70
     constructor(obj) {
-        Object.assign(this,obj)
+        // Convertis un objet en une entité Weather en appliquant uniquement les valeurs aux clés existantes
+        for (var property in obj) {
+            if (property == "datetime") {
+                this["timestamp"] = new Date(obj[property]).getTime() / 1000
+                continue
+            }
+            if (obj.hasOwnProperty(property) && this.hasOwnProperty(property))
+                this[property] = obj[property];
+        }
     }
 }

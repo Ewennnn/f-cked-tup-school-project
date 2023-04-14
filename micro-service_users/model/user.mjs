d01e786d171d77cbb5f4731e707f3ff06f0eefda
SteeZx <lespalmiersdeludon@gmail.com>
2023-04-14 11:48:12 +0200

    repatch user.mjs

--- micro-service_users/model/user.mjs
+++ micro-service_users/model/user.mjs
@@ -1,20 +1,26 @@
 'use strict'
 
 import Favorite from "./favorite.mjs"
-
+import bcrypt from 'bcryptjs'
 
 export default class User {
     login
     password
-    salt
     email
 
     favorites
     constructor(obj) {
         // Object.assign(this,obj)
         this.login = obj.login || ""
-        this.password = obj.password || ""
-        this.salt = obj.salt || ""
+        if (obj.password) {
+            let crypt = this.cryptPassword(obj.password)
+        this.password = crypt.hash
+        this.salt = crypt.salt 
+        } else {
+            this.password = ""
+            this.salt = ""
+        }
+        
         this.email = obj.email || ""
         if (obj.favorites)
             this.favorites = obj
@@ -23,4 +29,24 @@ export default class User {
         else
             this.favorites = []
     }
+
+    async cryptPassword(password){
+        const hash = ""
+        const salt  = ""
+        try {
+            salt = await bcrypt.genSalt()
+            hash = await bcrypt.hash(password, salt)
+            console.log(hash)
+        } catch (error) {
+            console.log(error.message)
+        }
+        return {hash, salt}
+    }
+    
+    setPassword(password){
+        let crypt = this.cryptPassword(password)
+        this.password = crypt.hash
+        this.salt = crypt.salt
+    }
+        
 }

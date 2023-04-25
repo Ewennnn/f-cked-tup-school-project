314d99ae1e4c048a2e05485cc9b6ec43ef7bd9c9
Ewennn <ewen.bosquet@gmail.com>
2023-04-25 21:16:05 +0200

    config all micro-services in encosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,21 @@ module.exports = {
     script : "./micro-service_meteo/startServer.mjs",
     watch: ["./micro-service_meteo"],
     watch_delay: 1000,
-    ignore_watch: ["node_modules"]
-}]
+    ignore_watch: ["node_modules", "./micro-service_meteo/node_modules"]
+  },
+  {
+    name: "ms-places",
+    script: "./micro-service_places/startServer.mjs",
+    watch: ["./micro-service_places"],
+    watch_delay: 1000,
+    ignore_watch: ["node_modules", "./micro-service_places/node_modules"]
+  },
+  {
+    name   : "ms-users",
+    script : "./micro-service_users/startServer.mjs",
+    watch: ["./micro-service_users"],
+    watch_delay: 1000,
+    ignore_watch: ["node_modules", "./micro-service_users/node_modules"]
+  }
+]
 }

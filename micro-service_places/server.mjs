0208e1423af915706646f70b135e12c7de5c4616
Devict <58944173+devictgit@users.noreply.github.com>
2023-05-08 18:10:44 +0200

    swagger premier essai

--- micro-service_places/server.mjs
+++ micro-service_places/server.mjs
@@ -1,7 +1,11 @@
 'use strict';
 
+import Joi from 'joi';
 import Hapi from '@hapi/hapi';
 import { placeController } from "./controller/placeController.mjs";
+import inert from '@hapi/inert';
+import Vision from '@hapi/vision'
+import HapiSwagger from 'hapi-swagger'
 import { ports } from '../microServices.config.mjs';
 
 const routes =[
@@ -15,6 +19,18 @@ const routes =[
     {
         method: 'GET',
         path: '/restaurants',
+        options: {
+            description: 'Get the restaurants around the location',
+            tags: ['api'],
+            validate: {
+                params: Joi.object({
+                    location: {
+                        latitude: Joi.number().required(),
+                        longitude: Joi.number().required()
+                    }
+                }).description('Required format : { latitude: number, longitude: number }')
+            }
+        },
         handler: async (request, h) => {
             //le message renvoyé et le code http
             return h.response(await placeController.findRestaurantsByLocation({lat: 47.218371, lng: -1.553621}, 3000)).code(200)
@@ -36,6 +52,14 @@ export const init = async () => {
 };
 
 export  const start = async () => {
+    await server.register([
+        inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: PlacesJoiConfig.swaggerOptions
+        }
+    ])
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
     return server;

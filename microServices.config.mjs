60a2b6802e4d7dc5ce806574f20be429e4a2dccd
Ewennn <ewen.bosquet@gmail.com>
2023-05-16 10:25:28 +0200

    add proxy agent and fetch function to use agent by default

--- microServices.config.mjs
+++ microServices.config.mjs
@@ -1,8 +1,24 @@
 'use strict'
 
+import { HttpsProxyAgent } from 'https-proxy-agent'
+import fetch from 'node-fetch'
+
 export const ports = {
     meteo: 3001,
     places: 3002,
     users: 3003,
     location: 3004
+}
+
+const proxy = process.env.https_proxy
+export const proxyAgent = proxy != null ? new HttpsProxyAgent(proxy) : null
+
+if (proxyAgent != null) {
+    console.log(`Proxy: ${proxy}`);
+} else {
+    console.warn("No proxy was found");
+}
+
+export default async function fetchUsingAgent(url) {
+    return proxyAgent != null ? await fetch(url, {agent: proxyAgent}) : await fetch(url)
 }

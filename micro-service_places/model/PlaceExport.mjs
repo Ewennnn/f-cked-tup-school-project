3638545d10f926d4ebac463a55523f5443e00163
Devict <58944173+devictgit@users.noreply.github.com>
2023-05-16 11:41:41 +0200

    fix: Ooops forgot to uncomment something

--- micro-service_places/model/PlaceExport.mjs
+++ micro-service_places/model/PlaceExport.mjs
@@ -2,7 +2,7 @@ import Joi from "joi";
 import PlacesExport, { placesExportModel } from "./PlacesExport.mjs";
 
 export const placeExportModel = placesExportModel.concat(Joi.object({
-  // formatted_address: Joi.string().required(),
+  formatted_address: Joi.string().required(),
   international_phone_number: Joi.string().required(),
   photos: Joi.array().items(Joi.object({
     height: Joi.number().required(),
@@ -21,24 +21,14 @@ export default class PlaceExport {
   user_rating_total
   vicinity
 
-  // formatted_address
+  formatted_address
   international_phone_number
   photos
 
   constructor(obj) {
     Object.assign(this, new PlacesExport(obj))
-    // this.place_id = obj.place_id;
-    // this.name = obj.name;
-    // this.location = {
-    //   latitude: obj.geometry.location.lat,
-    //   longitude: obj.geometry.location.lng,
-    // };
-    // this.rating = obj.rating;
-    // this.types = obj.types;
-    // this.user_rating_total = obj.user_ratings_total;
-    // this.vicinity = obj.vicinity;
     
-    // this.formatted_address = obj.formatted_address
+    this.formatted_address = obj.formatted_address
     this.international_phone_number = obj.international_phone_number
     this.photos = obj.photos.map(photo => ({
       height: photo.height,

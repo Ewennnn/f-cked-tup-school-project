ff73c592d2735a9f6f40379746cdeafc6ae9ad3c
SteeZx <lespalmiersdeludon@gmail.com>
2023-06-09 10:25:01 +0200

    Patch error (Uniquement sur Nexus 5X (API 33)

--- Android/app/src/main/java/com/example/bestdate/ApiClient.kt
+++ Android/app/src/main/java/com/example/bestdate/ApiClient.kt
@@ -10,23 +10,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
     private const val BASE_URL: String = "http://51.178.30.150/"
 
-    private val gson : Gson by lazy {
-        GsonBuilder().setLenient().create()
-    }
+    private val gson : Gson = GsonBuilder().setLenient().create()
 
-    private val httpClient : OkHttpClient by lazy {
-        OkHttpClient.Builder().build()
-    }
+    private val httpClient : OkHttpClient = OkHttpClient.Builder().build()
 
-    private val retrofit : Retrofit by lazy {
+    private val retrofit : Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
-    }
 
-    val apiService : ApiService by lazy{
-        retrofit.create(ApiService::class.java)
-    }
+    val apiService : ApiService =
+        retrofit.create(ApiService::class)
 }
